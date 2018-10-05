e20fa5f934b37bf95e90cbbeedd0a513ecbb78f5
Kit Jonathan Te <kjonathante@gmail.com>
2018-10-05 15:45:18 -0700

    Initial Webserver functionality

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # nodemcu-webserver-wifiap
 espruino-esp8266 html form to configure wifi connection
+
+
+### references:
+https://www.espruino.com/Posting+Forms
---
+++ main.js
@@ -0,0 +1,110 @@
+var wifi;
+// The last data that was POSTed to us
+var postData = {};
+
+// This serves up the webpage itself
+function sendPage(res) {
+  // We're using ES6 Template Literals here to make the HTML easy to read.
+  var d = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wifi Client Configuration</title>
+  </head>
+  <body>
+    <style>
+      label {
+        display: block;
+        padding: 5px;
+      }
+    </style>
+    <h1>Wifi Client Configuration</h1>
+    <form action="/" method="post">
+      <label for="ssid">Network Name
+        <input type="text" name="s" id="ssid">
+      </label>
+      <label for="password">Password
+        <input type="text" name="p" id="password">
+      </label>
+      <label for="nodecode">Node Code
+        <input type="text" name="c" id="nodecode">
+      </label>
+      <div>
+        <button>Save</button>
+      </div>
+    </form>    
+  </body>
+  </html>`;
+  res.writeHead(200, {
+    "Content-Type": "text/html",
+    "Content-Length": d.length
+  });
+  res.end(d);
+}
+
+// This handles the HTTP request itself and serves up the webpage or a
+// 404 not found page
+function onPageRequest(req, res) {
+  var a = url.parse(req.url, true);
+  if (a.pathname == "/") {
+    // handle the '/' (root) page...
+    // If we had a POST, handle the data we're being given
+    if (
+      req.method == "POST" &&
+      req.headers["Content-Type"] == "applicatio­n/x-www-form-urlencoded"
+    )
+      handlePOST(req, function() {
+        sendPage(res);
+      });
+    else sendPage(res);
+  } else {
+    // Page not found - return 404
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404: Page " + a.pathname + " not found");
+  }
+}
+
+// This handles any received data from the POST request
+function handlePOST(req, callback) {
+  var data = "";
+  req.on("data", function(d) {
+    data += d;
+  });
+  req.on("end", function() {
+    // All data received from the client, so handle the url encoded data we got
+    // If we used 'close' then the HTTP request would have been closed and we
+    // would be unable to send the result page.
+    postData = {};
+    data.split("&").forEach(function(el) {
+      var els = el.split("=");
+      postData[els[0]] = decodeURIComponent(els[1]);
+    });
+    // finally our data is in postData
+    console.log(postData);
+    // do stuff with it!
+    //
+    //
+    //
+    // call our callback (to send the HTML result)
+    callback();
+  });
+}
+
+function startWebserver() {
+  require("http")
+    .createServer(onPageRequest)
+    .listen(80);
+}
+
+function main() {
+  wifi.startAP("espruino-esp8266", {}, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    startWebserver();
+    return console.log("Sucessfully started AP");
+  });
+}
+
+main();
